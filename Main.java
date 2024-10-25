4bd661f11526e202a8f7f41e40d0e7c6cfff58fb
Frederik <142442517+FrederikJaeger@users.noreply.github.com>
2024-10-25 02:02:37 +0200

    Add files via upload
    Task 1

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Flow flow = new Flow();
+        flow.methodA();
+        flow.methodB();
+        flow.methodC();
+        flow.methodD();
+        flow.methodE();
+        flow.methodF();
+        flow.methodG();
+        flow.methodH();
+        flow.methodI();
+        flow.methodJ();
+        flow.methodK();
+        flow.methodL();
+        flow.methodM();
+    }
+}
